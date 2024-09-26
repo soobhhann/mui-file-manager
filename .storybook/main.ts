2228dc68ea516db241bc2ce2354e3f85b16e185f
soobhhann <soobhhann@gmail.com>
2024-09-26 23:50:31 +0330

    renew the config files

---
+++ .storybook/main.ts
@@ -0,0 +1,9 @@
+import { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  framework: "@storybook/react-vite",
+};
+
+export default config;
